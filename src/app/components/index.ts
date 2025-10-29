90e4461a6a9ae7fe9cb942011a919d6699553321
parksiwoon <parksw003@gmail.com>
2025-10-29 12:55:44 +0900

    ✨ feat: 컴포넌트들을 한 곳에서 export하는 index.ts 파일 추가

---
+++ src/app/components/index.ts
@@ -0,0 +1,11 @@
+// 컴포넌트들을 한 곳에서 export
+export { default as StatsCard } from './StatsCard';
+export { default as ParkingLotSelector } from './ParkingLotSelector';
+export { default as FloorSelector } from './FloorSelector';
+export { default as SearchBar } from './SearchBar';
+export { default as VehicleInfo } from './VehicleInfo';
+export { default as EntranceSelectionModal } from './EntranceSelectionModal';
+export { default as ParkingMap } from './ParkingMap';
+export { default as CameraModal } from './CameraModal';
+export { default as RealTimeUpdate } from './RealTimeUpdate';
+export { default as FloorInfoManager } from './FloorInfoManager';
